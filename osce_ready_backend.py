671a531101652088b19304bc852ea37dc359e352
R Scott <rredmondpics@gmail.com>
2025-09-25 12:04:41 +0100

    Fix Flask bind to Render PORT

--- osce_ready_backend.py
+++ osce_ready_backend.py
@@ -310,6 +310,7 @@ def callback():
 
 
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
